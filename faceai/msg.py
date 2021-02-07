4ec5e4ede9fbc9c9be260e5149443935b723b4a0
levidavis <47396371+levidavis@users.noreply.github.com>
2021-02-07 13:19:38 +0000

    Update msg.py

--- faceai/msg.py
+++ faceai/msg.py
@@ -1,13 +1,10 @@
-#fb email token from emailondeck bdaf37b61a3e20be2bf4ca7b6f5518df
-
 import telegram
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-token = "1559670838:AAF2s09xvuOQ_AEiXZgYWhZmneQ5He5gSBc"
+token = ""
 bot = telegram.Bot(token=token)
 
-imagesend = 'obama.jpg'
 
 def msg(text):
     # bot.send_photo('1345754285', open(imagesend,'rb'))
